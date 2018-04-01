29fb3800b929b51057dff8a1b9eefa4349396b3b
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2018-04-01 22:54:12 +0300

    connectedUsers showing

--- SocketManager.js
+++ SocketManager.js
@@ -1,6 +1,7 @@
 const { io } = require('./index.js');
 
-let connectedUsers = { };
+// let connectedUsers = { };
+const connectedUsers2 = [];
 
 const { VERIFY_USER, USER_CONNECTED, LOGOUT, USERS_CHANGED } = require('./Events');
 
@@ -9,8 +10,10 @@ const { createChat, createMessage, createUser } = require('./Factories');
 module.exports = (socket) => {
   console.log(`Socket Id: ${socket.id}`);
 
+  // socket.on(VERIFY_USER, (nickname, callback) => {
   socket.on(VERIFY_USER, (nickname, callback) => {
-    if (isUser(connectedUsers, nickname)) {
+    // if (isUser(connectedUsers, nickname)) {
+    if (isUser2(connectedUsers2, nickname)) {
       callback({
         isUser: true,
         user: null
@@ -25,40 +28,59 @@ module.exports = (socket) => {
   });
 
   socket.on(USER_CONNECTED, (user) => {
-    connectedUsers = addUser(connectedUsers, user);
+    // connectedUsers = addUser(connectedUsers, user);
+    addUser2(connectedUsers2, user);
 
     socket.user = user;
 
-    io.emit(USER_CONNECTED, connectedUsers);
+    io.emit(USER_CONNECTED, connectedUsers2);
 
     // bağlı kullanıcı listesi tekrar gönderiliyor
-    io.emit(USERS_CHANGED, connectedUsers);
-
-    console.log(connectedUsers);
+    io.emit(USERS_CHANGED, connectedUsers2);
   });
 
-  socket.on(LOGOUT, (username) => {
-    connectedUsers = removeUser(connectedUsers, username);
+  socket.on(LOGOUT, (user) => {
+    // connectedUsers = removeUser(connectedUsers, username);
+    removeUser2(connectedUsers2, user);
 
     // bağlı kullanıcı listesi tekrar gönderiliyor
-    io.emit(USERS_CHANGED, connectedUsers);
-
-    console.log(connectedUsers);
+    io.emit(USERS_CHANGED, connectedUsers2);
   });
 };
 
-function addUser(userList, user) {
-  const newList = Object.assign({}, userList);
-  newList[user.name] = user;
-  return newList;
+function isUser2(userList, username) {
+  let result = false;
+
+  userList.forEach((user) => {
+    if (user.name === username) {
+      result = true;
+    }
+  });
+
+  return result;
 }
 
-function isUser(userList, username) {
-  return username in userList;
+function addUser2(userList, user) {
+  userList.push(user);
 }
 
-function removeUser(userList, username) {
-  const newList = Object.assign({}, userList);
-  delete newList[username];
-  return newList;
+function removeUser2(userList, user) {
+  const index = userList.indexOf(user);
+  userList.splice(index, 1);
 }
+
+// function isUser(userList, username) {
+//   return username in userList;
+// }
+
+// function addUser(userList, user) {
+//   const newList = Object.assign({}, userList);
+//   newList[user.name] = user;
+//   return newList;
+// }
+
+// function removeUser(userList, username) {
+//   const newList = Object.assign({}, userList);
+//   delete newList[username];
+//   return newList;
+// }
